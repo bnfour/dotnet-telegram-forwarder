a4883b5693c60b5e40bb20b6356e9cfde1cb4b08
Bn4 <bnfour@bnfour.net>
2018-08-21 08:59:26 +0700

    added controller that is listening for requests for now

---
+++ WebToTelegramCore/Controllers/WebAndTelegramApisController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using WebToTelegramCore.Models;
+
+namespace WebToTelegramCore.Controllers
+{
+    /// <summary>
+    /// Controller that handles both web API and telegram API calls,
+    /// since they're both POST with JSON bodies.
+    /// </summary>
+    public class WebAndTelegramApisController : Controller
+    {
+        // POST /api
+        /// <summary>
+        /// Handles web API calls.
+        /// </summary>
+        /// <param name="request">Request object in POST request body.</param>
+        /// <returns>400 Bad request on malformed Requests, 200 OK otherwise.</returns>
+        [HttpPost, Route("api")]
+        public ActionResult HandleWebApi([FromBody] Request request)
+        {
+            // silently deny malformed requests
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            // accept and do nothing for now
+            // TODO actually handle request via separate service
+            // TODO return JSON response
+            return Ok();
+        }
+
+        // TODO telegram API webhook handling
+    }
+}
