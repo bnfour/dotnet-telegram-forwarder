20c56e470bd03c70fff140a3f4758a3699221a31
Bn4 <bnfour@bnfour.net>
2018-08-21 11:06:59 +0700

    RecordContext and an empty SQLite database added to app

--- WebToTelegramCore/Startup.cs
+++ WebToTelegramCore/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -27,6 +28,11 @@ namespace WebToTelegramCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // singleton makes changes to non-db properties persistent
+            services.AddDbContext<RecordContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")),
+                ServiceLifetime.Singleton, ServiceLifetime.Singleton);
 
             services.AddSingleton<ITokenGeneratorService, TokenGeneratorService>();
         }
