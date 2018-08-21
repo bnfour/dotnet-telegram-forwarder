b8089675e490092e3a420baeacd166eaed718ffc
Bn4 <bnfour@bnfour.net>
2018-08-21 12:47:18 +0700

    interface to send messages via telegram implemented

---
+++ WebToTelegramCore/Services/ITelegramBotService.cs
@@ -0,0 +1,16 @@
+﻿namespace WebToTelegramCore.Services
+{
+    /// <summary>
+    /// Interface that exposes greatly simplified version of Telegram bot API.
+    /// The only method allows sending of text messages.
+    /// </summary>
+    public interface ITelegramBotService
+    {
+        /// <summary>
+        /// Sends text message. Markdown formatting should be supported.
+        /// </summary>
+        /// <param name="accountId">ID of account to send message to.</param>
+        /// <param name="message">Text of the message.</param>
+        void Send(int accountId, string message);
+    }
+}
