91ec5ba0143ad24753cb9a58d53d4a9f24696563
Bn4 <bnfour@bnfour.net>
2018-08-21 08:42:32 +0700

    added Request class

---
+++ WebToTelegramCore/Models/Request.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebToTelegramCore.Models
+{
+    /// <summary>
+    /// Represents user's request to our web API.
+    /// </summary>
+    public class Request
+    {
+        /// <summary>
+        /// Auth token.
+        /// </summary>
+        [Required, RegularExpression(@"[a-zA-Z0-9+=]{16}")]
+        public string Token { get; set; }
+
+        /// <summary>
+        /// Message to send. Max size is hardcoded to 8 KB.
+        /// </summary>
+        [Required, StringLength(8192)]
+        public string Message { get; set; }
+    }
+}
