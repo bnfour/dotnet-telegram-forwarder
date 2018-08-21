8cfcc11b113116875cc512fab1a99dcd0c8c9cbe
Bn4 <bnfour@bnfour.net>
2018-08-21 10:40:55 +0700

    database context for records implemented

---
+++ WebToTelegramCore/RecordContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using WebToTelegramCore.Models;
+
+namespace WebToTelegramCore
+{
+    /// <summary>
+    /// Class that encapsulates Records for EF usage.
+    /// </summary>
+    public class RecordContext : DbContext
+    {
+        /// <summary>
+        /// Record collection for manipulation within the app.
+        /// </summary>
+        public DbSet<Record> Records { get; private set; }
+
+        /// <summary>
+        /// Constructor that does literally nothing
+        /// but is required anyway due to base call.
+        /// </summary>
+        /// <param name="options">Options for the context.</param>
+        public RecordContext(DbContextOptions<RecordContext> options) : base(options) { }
+
+        /// <summary>
+        /// Sets model parameters: makes UsageCounter and LastSuccessTimestamp .NET-only
+        /// properties, marks token as a primary key.
+        /// </summary>
+        /// <param name="modelBuilder">ModelBuilder to use.</param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Record>()
+                .ToTable("Records")
+                .Ignore(r => r.UsageCounter)
+                .Ignore(r => r.LastSuccessTimestamp)
+                .HasKey(r => r.Token);
+        }
+    }
+}
