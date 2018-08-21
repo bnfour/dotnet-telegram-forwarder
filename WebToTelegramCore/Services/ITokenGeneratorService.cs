16ec37e5d46622998540205f501763cfc55ba012
Bn4 <bnfour@bnfour.net>
2018-08-21 07:59:55 +0700

    interface for token generator added

---
+++ WebToTelegramCore/Services/ITokenGeneratorService.cs
@@ -0,0 +1,18 @@
+﻿namespace WebToTelegramCore.Services
+{
+    /// <summary>
+    /// Interface to services that generate auth tokens.
+    /// </summary>
+    public interface ITokenGeneratorService
+    {
+        // Tokens are strings of length 16 which contain letters a to z, both
+        // uppercase and lowercase, digits zero to nine and also + and = symbols
+        // for a total of 64
+
+        /// <summary>
+        /// Generates a new token.
+        /// </summary>
+        /// <returns>Generated token as a string.</returns>
+        string Generate();
+    }
+}
