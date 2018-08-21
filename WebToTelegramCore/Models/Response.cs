6ca8dd81a8b0e2d41395b180604be90dbfacfb6d
Bn4 <bnfour@bnfour.net>
2018-08-21 09:08:07 +0700

    Response class added

---
+++ WebToTelegramCore/Models/Response.cs
@@ -0,0 +1,24 @@
+﻿namespace WebToTelegramCore.Models
+{
+    /// <summary>
+    /// Represents web API response to user.
+    /// Also in JSON, just like the initial request.
+    /// </summary>
+    public class Response
+    {
+        /// <summary>
+        /// Quick indicator of whether request was accepted.
+        /// </summary>
+        public bool Ok { get; set; }
+
+        /// <summary>
+        /// Machine-readable error code.
+        /// </summary>
+        public int Code { get; set; }
+
+        /// <summary>
+        /// Human-readable description of error.
+        /// </summary>
+        public string Details { get; set; }
+    }
+}
