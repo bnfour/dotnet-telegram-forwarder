e142b6cd7342bf7546db3cf8ae92f153ab97cb0f
Bn4 <bnfour@bnfour.net>
2018-08-22 12:40:09 +0700

    interface to web api implemented

---
+++ WebToTelegramCore/Services/IOwnApiService.cs
@@ -0,0 +1,17 @@
+﻿using WebToTelegramCore.Models;
+
+namespace WebToTelegramCore.Services
+{
+    /// <summary>
+    /// Interface to implement non-Telegram web API handling.
+    /// </summary>
+    public interface IOwnApiService
+    {
+        /// <summary>
+        /// Method to handle incoming request from the web API.
+        /// </summary>
+        /// <param name="request">Request to handle.</param>
+        /// <returns>Response to the request, ready to be returned to client.</returns>
+        Response HandleRequest(Request request);
+    }
+}
