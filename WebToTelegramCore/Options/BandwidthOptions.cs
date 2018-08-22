958814f949101833c0051737cfc55302c3323f0e
Bn4 <bnfour@bnfour.net>
2018-08-22 15:31:40 +0700

    bandwidth-related options class implemented: default values added to appconfig.json

---
+++ WebToTelegramCore/Options/BandwidthOptions.cs
@@ -0,0 +1,20 @@
+﻿namespace WebToTelegramCore.Options
+{
+    /// <summary>
+    /// Class that represents app's setting related to message
+    /// throughput limitations.
+    /// </summary>
+    public class BandwidthOptions
+    {
+        /// <summary>
+        /// Represents initial number of messages available.
+        /// </summary>
+        public int InitialCount { get; set; }
+
+        /// <summary>
+        /// Represents amount of seconds after last message send to regenerate
+        /// one message.
+        /// </summary>
+        public int SecondsPerRegeneration { get; set; }
+    }
+}
