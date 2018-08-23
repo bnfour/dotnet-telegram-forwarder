e1b9a05edfed8ea1423449b195b6677ba49334d1
Bn4 <bnfour@bnfour.net>
2018-08-23 14:46:25 +0700

    interface for bot commands implemented

---
+++ WebToTelegramCore/BotCommands/IBotCommand.cs
@@ -0,0 +1,25 @@
+﻿using WebToTelegramCore.Models;
+
+namespace WebToTelegramCore.BotCommands
+{
+    /// <summary>
+    /// Interface to implement various bot commands without arguments.
+    /// Used as text-only command interface.
+    /// </summary>
+    public interface IBotCommand
+    {
+        /// <summary>
+        /// Text that should be used to invoke the command.
+        /// Leading forward slash should be included here.
+        /// </summary>
+        string Command { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="record">Record associated with user who sent teh command,
+        /// or null if user has no Record (have not received the token).</param>
+        /// <returns>Message to send back to user. Markdown is supported.</returns>
+        string Process(Record record);
+    }
+}
