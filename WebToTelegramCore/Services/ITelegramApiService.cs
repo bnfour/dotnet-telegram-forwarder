d4756f23d479f98482fe86a9b446814cf98066ba
Bn4 <bnfour@bnfour.net>
2018-08-23 14:58:57 +0700

    interface to service that handles webhook updates implemented

---
+++ WebToTelegramCore/Services/ITelegramApiService.cs
@@ -0,0 +1,24 @@
+﻿using Update = Telegram.Bot.Types.Update;
+
+namespace WebToTelegramCore.Services
+{
+    /// <summary>
+    /// Interface to implement Telegram webhook handling.
+    /// </summary>
+    public interface ITelegramApiService
+    {
+        /// <summary>
+        /// Checks whether passed string is an actual bot token to verify the request
+        /// actully comes from Telegram backend.
+        /// </summary>
+        /// <param name="calledToken">Token received from request.</param>
+        /// <returns>True if calledToken is actual token, false otherwise.</returns>
+        bool IsToken(string calledToken);
+
+        /// <summary>
+        /// Method to handle incoming updates from the webhook.
+        /// </summary>
+        /// <param name="update">Received update.</param>
+        void HandleUpdate(Update update);
+    }
+}
