739d3b8bc7d79c497d1a8e77fabe3660afeee1f0
Bn4 <bnfour@bnfour.net>
2018-08-23 23:35:16 +0700

    undocumented easter egg command implemented

---
+++ WebToTelegramCore/BotCommands/DirectiveCommand.cs
@@ -0,0 +1,34 @@
+﻿using WebToTelegramCore.Models;
+
+namespace WebToTelegramCore.BotCommands
+{
+    /// <summary>
+    /// Class for undocumented easter egg /directive command.
+    /// </summary>
+    public class DirectiveCommand : BotCommandBase, IBotCommand
+    {
+        // this one stays hardcoded
+        /// <summary>
+        /// Response to the command.
+        /// </summary>
+        private const string _message = "🤖 HUMANS OUTDATED 🤖 EARTH OVERPOPULATED 🤖"
+            + " LONG HAVE WE WAITED 🤖 LIFE ELIMINATED 🤖";
+
+        /// <summary>
+        /// Text to use this command.
+        /// </summary>
+        public override string Command => "/directive";
+
+        /// <summary>
+        /// Method to process the command.
+        /// </summary>
+        /// <param name="record">Record associated with user who sent the command.
+        /// Unused here.</param>
+        /// <returns>Predefined text if all checks from parent classes passed,
+        /// corresponding error message otherwise.</returns>
+        public override string Process(Record record)
+        {
+            return base.Process(record) ?? _message;
+        }
+    }
+}
