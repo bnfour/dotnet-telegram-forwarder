b9dac97fcaf27dd38cfeb21e48396bf770a0c61e
Bn4 <bnfour@bnfour.net>
2018-08-29 00:47:04 +0700

    basic port change implemented (i won't use it, so quick and dirty implementation will do)

--- WebToTelegramCore/Program.cs
+++ WebToTelegramCore/Program.cs
@@ -5,15 +5,27 @@ namespace WebToTelegramCore
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            // hardcoded default
+            int port = 8082;
+
+            if (args.Length == 2 && args[0].Equals("--port")
+                && System.Int32.TryParse(args[1], out int nonDefPort))
+            {
+                port = nonDefPort;
+            }
+
+            return WebHost.CreateDefaultBuilder(args)
                 .UseKestrel()
-                .UseUrls("http://localhost:8082")
+                .UseUrls($"http://localhost:{port}")
                 .UseStartup<Startup>();
+        }
     }
 }
