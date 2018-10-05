61e4b3dc2aebd1c9645046715b0ca75eb03af7dd
Bn4 <bnfour@bnfour.net>
2018-10-05 12:07:08 +0700

    FormatterSerice probably implemented

--- WebToTelegramCore/Services/FormatterService.cs
+++ WebToTelegramCore/Services/FormatterService.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
+using WebToTelegramCore.FormatterHelpers;
 
 namespace WebToTelegramCore.Services
 {
@@ -11,6 +10,11 @@ namespace WebToTelegramCore.Services
     public class FormatterService : IFormatterService
     {
         /// <summary>
+        /// Instance of MarkdownTransformer to use.
+        /// </summary>
+        private readonly MarkdownTransformer _transformer = new MarkdownTransformer();
+
+        /// <summary>
         /// Transforms Markdown to HTML. Expects Markdown like Telegram clients do:
         /// **bold** and __italic__ are main differences from the CommonMark.
         /// </summary>
@@ -18,7 +22,37 @@ namespace WebToTelegramCore.Services
         /// <returns>Text with same format but in HTML supported by Telegram's API.</returns>
         public string TransformToHtml(string TgFlavoredMarkdown)
         {
-            throw new NotImplementedException();
+            // TODO think how to reuse these
+            TextWriter writer = new StringWriter();
+            var renderer = new Markdig.Renderers.HtmlRenderer(writer);
+
+            renderer = new Markdig.Renderers.HtmlRenderer(writer)
+            {
+                ImplicitParagraph = true
+            };
+
+            // replacing default renderes with our "fixed" versions
+            var toRemove = renderer.ObjectRenderers.Where(x =>
+                x.GetType() == typeof(Markdig.Renderers.Html.CodeBlockRenderer) ||
+                x.GetType() == typeof(Markdig.Renderers.Html.Inlines.LinkInlineRenderer))
+            .ToList();
+            foreach (var r in toRemove)
+            {
+                renderer.ObjectRenderers.Remove(r);
+            }
+            renderer.ObjectRenderers.Add(new NonNestedHtmlLinkInlineRenderer());
+            renderer.ObjectRenderers.Add(new TweakedCodeBlockRenderer());
+
+            // setting up rendering pipeline
+            var pipeline = new Markdig.MarkdownPipelineBuilder().Build();
+            pipeline.Setup(renderer);
+
+            // actually rendering the HTML
+            var cm = _transformer.ToCommonMark(TgFlavoredMarkdown);
+            var doc = Markdig.Parsers.MarkdownParser.Parse(cm);
+            renderer.Render(doc);
+            writer.Flush();
+            return writer.ToString();
         }
     }
 }
