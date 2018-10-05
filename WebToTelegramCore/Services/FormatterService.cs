d485ac9379ac2f63f856ff22f1f6dc01d9567ce9
Bn4 <bnfour@bnfour.net>
2018-10-05 11:27:34 +0700

    Placeholders for FormatterService implemented

---
+++ WebToTelegramCore/Services/FormatterService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebToTelegramCore.Services
+{
+    /// <summary>
+    /// Class that converts Markdown to HTML.
+    /// </summary>
+    public class FormatterService : IFormatterService
+    {
+        /// <summary>
+        /// Transforms Markdown to HTML. Expects Markdown like Telegram clients do:
+        /// **bold** and __italic__ are main differences from the CommonMark.
+        /// </summary>
+        /// <param name="TgFlavoredMarkdown">Markdown-formatted text.</param>
+        /// <returns>Text with same format but in HTML supported by Telegram's API.</returns>
+        public string TransformToHtml(string TgFlavoredMarkdown)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
