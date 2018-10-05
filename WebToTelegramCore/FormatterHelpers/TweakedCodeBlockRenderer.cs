e2cff42c5e92afb8e9d28214f46f7b6aa80beaea
Bn4 <bnfour@bnfour.net>
2018-10-05 11:39:37 +0700

    custom code block renderer implemented

---
+++ WebToTelegramCore/FormatterHelpers/TweakedCodeBlockRenderer.cs
@@ -0,0 +1,40 @@
+﻿using Markdig.Renderers;
+using Markdig.Syntax;
+
+namespace WebToTelegramCore.FormatterHelpers
+{
+    /// <summary>
+    /// Class that renders multiline code blocks inside &lt;pre&gt; tags only
+    /// as requested by Telegram's API docs,
+    /// opposed to default &lt;pre&gt; and &lt;code&gt;.
+    /// </summary>
+    public class TweakedCodeBlockRenderer : Markdig.Renderers.Html.CodeBlockRenderer
+    {
+        /// <summary>
+        /// Method to render the code block.
+        /// </summary>
+        /// <param name="renderer">Renderer to use.</param>
+        /// <param name="obj">Code block to render.</param>
+        protected override void Write(HtmlRenderer renderer, CodeBlock obj)
+        {
+            renderer.EnsureLine();
+            // all code related to <div> was wiped out as it's unused here anyway
+            if (renderer.EnableHtmlForBlock)
+            {
+                renderer.Write("<pre");
+                renderer.WriteAttributes(obj);
+                renderer.Write(">");
+            }
+
+            renderer.WriteLeafRawLines(obj, true, true);
+
+            if (renderer.EnableHtmlForBlock)
+            {
+                renderer.WriteLine("</pre>");
+            }
+
+            renderer.EnsureLine();
+
+        }
+    }
+}
