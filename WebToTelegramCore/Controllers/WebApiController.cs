954da12bbe3f8e1ad4575b16f2085d3bd2912239
Bn4 <bnfour@bnfour.net>
2021-12-12 13:07:03 +0700

    dual-purpose controller split in two, api methods converted to async, mostly final error handling in Telegram API controller

---
+++ WebToTelegramCore/Controllers/WebApiController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebToTelegramCore.Models;
+using WebToTelegramCore.Services;
+
+namespace WebToTelegramCore.Controllers
+{
+    public class WebApiController : Controller
+    {
+        /// <summary>
+        /// Field to store injected web API service.
+        /// </summary>
+        private readonly IOwnApiService _ownApi;
+
+        public WebApiController(IOwnApiService ownApi)
+        {
+            _ownApi = ownApi;
+        }
+
+        // POST /api
+        /// <summary>
+        /// Handles web API calls.
+        /// </summary>
+        /// <param name="request">Request object in POST request body.</param>
+        /// <returns>400 Bad request on malformed Requests,
+        /// 200 OK with corresponding Response otherwise.</returns>
+        [HttpPost, Route("api")]
+        // TODO drop response from return type
+        public async Task<ActionResult<Response>> HandleWebApi([FromBody] Request request)
+        {
+            // silently deny malformed requests
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            return _ownApi.HandleRequest(request);
+        }
+    }
+}
